6c9253462d33f0271d8c8e7c80e78709f91fcafd
RyanMiskiv <ryan.miskiv@gmail.com>
2018-09-10 14:12:29 -0600

    Homework Week 3

---
+++ hw-3/sketch.js
@@ -0,0 +1,16 @@
+//Setup function decleared
+function setup(){
+
+  //canvas 600px by 400px created
+  createCanvas(600,400);
+
+  //set background color to red
+  background('red');
+}
+
+//Draw function declared
+function draw(){
+
+  // using text, writing "Hello World", located close to the center of the canvas
+  text('Hello World!',300,200)
+}
