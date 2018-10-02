127dcb3a99b8e4939acb5218933d0d6d20ae96d2
RyanMiskiv <ryan.miskiv@gmail.com>
2018-10-01 23:36:29 -0600

    more improvements on hw-6

--- hw-6-test2/sketch.js
+++ hw-6-test2/sketch.js
@@ -1,32 +1,52 @@
 var ball_x;
 var ball_y;
-var ball_width = 80;
-var max_ball_mvt = 10;
+var ball_width = 20;
+var max_ball_mvt = 20;
 var xc = ball_X;
 var yc = ball_y;
 function setup() {
     createCanvas( windowWidth, windowHeight );
     // createCanvas( windowWidth, 600 );
     background(255);
+    translate(width/2,height/2);
 
-    ball_x = width * 0.5;
-    ball_y = height * 0.5;
+    push();
+    fill(0);
+    triangle(-width,-height,width,height,width,-height);
+    // rect(-width,-height,width * 2,height);
+    pop();
+
+    ball_x = 0;
+    ball_y = 0;
+
     frameRate(30);
 }
 
 function draw() {
+  translate(width/2,height/2);
 
-  // xc = constrain(ball_X, 0, width);
-  // xy = constrain(ball_Y, 0, height);
-    // draw the ball
-    fill( 'rgb(7, 180, 4)' );
-    strokeWeight(5);
-    ellipse(ball_x, ball_y, ball_width, ball_width);
+  rectMode(RADIUS);
+  strokeWeight(5);
+    // noStroke();
+    // ellipse(ball_x, ball_y, ball_width, ball_width);
 
-    // we want to randomly update the position of the ball
+
+    push();
+    fill(0);
+    stroke(255);
+    rect(ball_x,ball_y,ball_width,ball_width,10);
+    pop();
+
+    push();
+    fill(255);
+    stroke(0);
+    rect(-ball_x,-ball_y,ball_width,ball_width,10);
+    pop();
+
+    // updating position
     ball_x += random(-max_ball_mvt, max_ball_mvt);
     ball_y += random(-max_ball_mvt, max_ball_mvt);
-    // ball_width += ball_width%3;
-    map(ball_x,-width,width,0,width);
-    map(ball_y,-height,height,0,height);
+
+    map(ball_x,-width -100 ,width + 100,-width + ball_width, width - ball_width);
+    map(ball_y,-height -100,height + 100, 0, height - ball_width);
 }
