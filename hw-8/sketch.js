dbfd59c1cddd60dafc373154b5d76f4b5c3dca9b
RyanMiskiv <ryan.miskiv@gmail.com>
2018-10-17 14:03:58 -0600

    hw-8 progress

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -15,20 +15,22 @@ function setup(){
 
 function draw(){
   strokeWeight(3);
+  let density = map(mouseY,0,height, 20,70);
+  spacing = map(mouseX,0,width,15,30);
   //default color set
   if(mouseIsPressed == false){
     stroke('black');
     background('white');
     }
+    else{
+      stroke('white');
+      background('black');
+    }
 
     //lines
     for(let i = 0;i<=height; i+= spacing){
-      line(0,i,width/2,i + 50);
-      line(width,i,width/2, i +50);
+      line(0,i,width/2,i + density);
+      line(width,i,width/2, i + density);
     }
 
-}
-function mousePressed(){
-  stroke('white');
-  background('black');
 }
