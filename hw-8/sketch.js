2fcc7626f943df06f9a444a2df5006002c34b879
RyanMiskiv <ryan.miskiv@gmail.com>
2018-10-17 13:00:53 -0600

    more hw-8 progress

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -4,24 +4,31 @@ For Loop sketch
 Ryan Miskiv
 Creative Coding 1
 */
+
+//global variables
 var spacing = 15;
 
 function setup(){
   createCanvas(windowWidth,windowHeight);
-  stroke(10);
+
 }
 
 function draw(){
+  strokeWeight(3);
+  //default color set
   if(mouseIsPressed == false){
-    fill('black');
+    stroke('black');
     background('white');
     }
+
+    //lines
     for(let i = 0;i<=height; i+= spacing){
       line(0,i,width/2,i + 50);
       line(width,i,width/2, i +50);
     }
+
 }
 function mousePressed(){
-  fill('white');
+  stroke('white');
   background('black');
 }
