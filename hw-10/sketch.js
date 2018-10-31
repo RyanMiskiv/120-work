6d8d800ca80605f441b693c0854fc9d7fd05b71d
RyanMiskiv <ryan.miskiv@gmail.com>
2018-10-31 14:44:10 -0600

    hw-10 progress

--- hw-10/sketch.js
+++ hw-10/sketch.js
@@ -1,56 +1,65 @@
 var pos = {};
 pos.rect_x = 0;
 pos.rect_y = 0;
+//TODO: Get some of these variables in an array
+var rectSettings =[];
 var rect_width = 20;
 var max_rect_mvt = 20;
 
 function setup() {
     createCanvas( windowWidth, windowHeight );
-    // createCanvas( windowWidth, 600 );
+
     background(255);
     translate(width/2,height/2);
+    frameRate(24);
 
+    //creating background
     push();
     fill(0);
-    // triangle(-width,-height,width,height,width,-height);
+
     rect(-width,-height,width * 2,height);
     pop();
 
-    frameRate(24);
+
 }
 
 function draw() {
   translate(width * 0.5,height * 0.5);
+
 //Settings for the rectangles
   rectMode(RADIUS);
   strokeWeight(5);
 
-//drawing the rectangles
-    push();
-    fill(0);
-    stroke(255);
-    rect(pos.rect_x,pos.rect_y,rect_width,rect_width,10);
-    pop();
 
-    push();
-    fill(255);
-    stroke(0);
-    rect(-pos.rect_x,-pos.rect_y,rect_width,rect_width,10);
-    pop();
+    obj();
+    move();
+    bound();
 
-    // updating position
-    pos.rect_x += floor(random(-max_rect_mvt, max_rect_mvt));
-    pos.rect_y += floor(random(-max_rect_mvt, max_rect_mvt));
+}
 
-    //setting boundaries
-    if(pos.rect_x < -width){
-      pos.rect_x = 0;
-    }
-    if(pos.rect_y < -height) {
-      pos.rect_y = 0;
-    }
-    map(pos.rect_x,-width -80 ,width + 80,-width + rect_width, width - rect_width);
-    map(pos.rect_y,-height -80,height + 80, 0, height - rect_width);
-    pos.rect_x = constrain(pos.rect_x,-width/2 + 20,width/2 - 20);
-    pos.rect_y = constrain(pos.rect_y,-height/2 + 20,height/2 - 20 );
+function move(){
+  // updating position
+  pos.rect_x += floor(random(-max_rect_mvt, max_rect_mvt));
+  pos.rect_y += floor(random(-max_rect_mvt, max_rect_mvt));
+}
+
+function bound(){
+  //setting boundaries
+  pos.rect_x = constrain(pos.rect_x,-width/2 + 20,width/2 - 20);
+  pos.rect_y = constrain(pos.rect_y,-height/2 + 20,height/2 - 20 );
+}
+
+function obj(){
+  //drawing the rectangles
+      push();
+      fill(0);
+      stroke(255);
+      rect(pos.rect_x,pos.rect_y,rect_width,rect_width,10);
+      pop();
+
+      push();
+      fill(255);
+      stroke(0);
+      rect(-pos.rect_x,-pos.rect_y,rect_width,rect_width,10);
+      pop();
 }
