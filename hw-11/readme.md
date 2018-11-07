5ce888ab9534a924adc7d1cc6c6d8a9b2a54296f
RyanMiskiv <ryan.miskiv@gmail.com>
2018-11-06 19:35:23 -0700

    finished hw-11

--- hw-11/readme.md
+++ hw-11/readme.md
@@ -3,3 +3,9 @@ Ryan Miskiv
 [Class Sketch](https://ryanmiskiv.github.io/120-work/hw-11/)
 
 # Homework 11 Response
+
+This week I undertook probably the most difficult ambitious thing that I have done. It had a lot more moving parts and things than anything that I've built in the past and I must say I struggled a good bit with figuring out how to formulate all of this and get it to work as I envisioned it. I created little 'Caterpillar' guys that will run around the canvas.
+
+The problem that I was presented with first was my object just not being able to be created and crashing the entire code, and to fix this I actually used the console in Chrome for once because I couldn't locate the issue myself, and found I just wasn't declaring the for loop variable correctly. The other logic part I had a hard time with was getting each segment to update individually and not have them be just long static lines, which I achieved through creating each circle as an object with all of its own data that is then accessed by a larger class which keeps it all together. I thought about going in for lab hours, but I was able to figure my problems out myself, although it took a lot of trial and error. I also struggled with getting the color randomization to work properly with passing the randomized data to the Caterpillar class, but I referenced some of this weeks examples to discover the color() function, which I didn't realize was a thing and I was trying (and failing) to use the rbg function and randomize each value individually as I did once as an experiment for week 6 that I didn't end up using.
+
+Overall I'm pretty proud with the results of my work this week.
--- hw-11/sketch.js
+++ hw-11/sketch.js
@@ -3,26 +3,37 @@ Ryan Miskiv
 Creative Coding 1
 Week 11
 */
-var initial;
+
+//settings for the array of caterpillars
+let caterpillars = [];
+let max = 25;
+
+
+//TODO: Make this able to make a ton of Caterpillar of various colors, sizes, and directions
 function setup(){
 
   createCanvas(windowWidth,windowHeight);
-  frameRate(60);
-  background(0);
- // initial = new Caterpillar(width/2,height/2);
- initial = new Caterpillar(width/4,height/4,8,'rgb(14, 255, 1)');
+  background('rgb(114, 230, 233)');
+
+ for(let i = 0;i<max;i++){
+
+   caterpillars.push(new Caterpillar(width/2, height/2, random(5,10), color(random(255) , random(255), random(255)), random(-5,5), random(-5,5)));
+ }
 }
 
 function draw(){
-    background(0);
-    initial.frame();
+
+    background('rgb(114, 230, 233)');
+    // initial.frame();
+    for(let i = 0; i<max;i++){
+      caterpillars[i].frame();
+    }
 
 }
 
-function mousePressed(){
-  new Caterpillar(mouseX,mouseY);
-}
+//store all the data for the circle segments within an object, to make things easier.
 class Circle{
+
   constructor(x,y,width,deltaX,deltaY){
     this.x = x;
     this.y = y;
@@ -32,65 +43,68 @@ class Circle{
     this.deltaY = deltaY;
   }
 
-
 }
 
 class Caterpillar{
-  constructor(x,y,numOfSegs,color){
+
+  constructor(x,y,numOfSegs,color,deltaX,deltaY){
+
+//building the object, setting all the different variables we need
   this.posX = x;
   this.posY = y;
   this.color = color;
-  this.deltaX = 5;
-  this.deltaY = 5;
+  this.deltaX = deltaX;
+  this.deltaY = deltaY;
   this.numOfSegs = numOfSegs;
   this.segments = [];
   this.select = 0;
-  this.separation = 40;
+  this.separationX = this.deltaX * 4;
+  this.separationY = this.deltaY * 4;
+
   //fill the array with the individual circle segment objects
   for(let i=0;i<this.numOfSegs;i++){
-    this.segments.push(new Circle(this.posX - this.separation,this.posY - this.separation, 50 ,this.deltaX,this.deltaY));
-    this.separation += 20;
+    this.segments.push(new Circle(this.posX - this.separationX,this.posY - this.separationY, 40 ,this.deltaX,this.deltaY));
+    this.separationX += this.deltaX * 4;
+    this.separationY += this.deltaY * 4;
+
     }
 
   }
-
+//actually go through and draw each individual segment
   display(){
+
     push();
+
     fill(this.color);
     noStroke();
-    // translate(this.posX,this.posY);
+
     for(let i=0;i<this.numOfSegs;i++){
       ellipse(this.segments[i].x,this.segments[i].y,this.segments[i].width);
     }
     pop();
+
   }
-//TODO: Figure out how to make these parts move in a delayed fashion
+//update the selected segment with the delta
   move(){
 
     this.segments[this.select].x += this.segments[this.select].deltaX;
     this.segments[this.select].y += this.segments[this.select].deltaY;
+
   }
 
-
+//ensure that the caterpillar will bounce off the boundaries
   edgeCheck() {
-        // check if the ball has hit a vertical wall (left or right walls)
+
+        // check if the selected segment has hit a vertical wall (left or right walls)
         if (this.segments[this.select].x + (this.segments[this.select].width /2) >= width || this.segments[this.select].x - (this.segments[this.select].width /2) <= 0) {
             this.segments[this.select].deltaX *= -1;
         }
-        // check if the ball has hit a horizontal wall (top or bottom walls)
+        // check if the selected segment has hit a horizontal wall (top or bottom walls)
         if (this.segments[this.select].y + (this.segments[this.select].width /2) >= height || this.segments[this.select].y - (this.segments[this.select].width /2) <= 0) {
             this.segments[this.select].deltaY *= -1;
         }
     }
-  // frame(){
-  //   this.display();
-  //   this.move();
-  //   this.select++;
-  //   if(this.select == numOfSegs){
-  //     this.select = 0;
-  //   }
-  //   this.edgeCheck();
-  // }
+//Take everything and apply it to each individual segment. Everything occurs in a single frame
   frame(){
     for(let i = 0;i<this.numOfSegs;i++){
       this.display();
