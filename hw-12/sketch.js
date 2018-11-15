f60beb08a94409b61fca7dcde2b4db110d74ff73
RyanMiskiv <ryan.miskiv@gmail.com>
2018-11-14 18:09:12 -0700

    finishing touches - comments

--- hw-12/sketch.js
+++ hw-12/sketch.js
@@ -6,6 +6,7 @@ function setup(){
 
   createCanvas(windowWidth, windowHeight);
     background('black');
+    //create an array full of smiles with randomized settings
     for(let i = 0; i < max; i++){
       smiles.push(new smile(random(150, width - 150), random(150, height - 150), random(25, 150), random(-5,5), random(-5,5)));
     }
@@ -14,6 +15,7 @@ function setup(){
 function draw(){
 
   background('black');
+  //move through each smiley and have it move as well as make sure it's not hitting any walls or other smileys
   for(let i = 0; i < max; i++){
     smiles[i].frame();
     smiles[i].collision(smiles, i);
--- hw-12/smile.js
+++ hw-12/smile.js
@@ -1,5 +1,6 @@
 class smile{
   constructor(x,y,width,deltaX,deltaY){
+    //main variables assigned from input
     this.posX = x;
     this.posY = y;
     this.width = width;
@@ -8,13 +9,15 @@ class smile{
     this.rad = this.width/2;
   }
   display(){
+    //draw the actual smiley
     push();
     noFill();
     stroke('white');
-    // translate(this.posX, this.posY);
+    //main circle
     ellipse(this.posX,this.posY,this.width);
+    //smile
     arc(this.posX,this.posY,this.width/1.5,this.width/1.5, PI/8 , 7*PI/8);
-
+    //eyes
     strokeWeight(3);
     point(this.posX - this.width/7, this.posY - this.width/7);
     point(this.posX + this.width/7, this.posY - this.width/7);
@@ -26,22 +29,21 @@ class smile{
     this.posY += this.deltaY;
   }
   edgeCheck() {
-       // check if the ball has hit a vertical wall (left or right walls)
+       // check if the smile has hit a vertical wall (left or right walls)
        if (this.posX + this.rad >= width || this.posX - this.rad <= 0) {
            this.deltaX *= -1;
 
        }
-       // check if the ball has hit a horizontal wall (top or bottom walls)
+       // check if the smile has hit a horizontal wall (top or bottom walls)
        if (this.posY + this.rad >= height || this.posY - this.rad <= 0) {
            this.deltaY *= -1;
 
        }
      }
     collision(otherSmiles, myId){
-      // for loop touches each of the balls in the array
+      // select each of the smiles in the array
         for (let n = 0; n < otherSmiles.length; n++) {
-            // if n != myId, then check for touching
-            // otherwise, its ME and we need to skip
+            // if n != myId, then check for collision otherwise, its ME and we need to not check it
             if (n != myId) {
                 let d = dist(this.posX, this.posY, otherSmiles[n].posX, otherSmiles[n].posY);
                 let combinedR = this.rad + otherSmiles[n].rad;
