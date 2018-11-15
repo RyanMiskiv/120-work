719043cc7c63d7f60aa646a30b0bddc4bc1167ad
RyanMiskiv <ryan.miskiv@gmail.com>
2018-11-14 17:50:57 -0700

    hw-12 progress

--- hw-12/sketch.js
+++ hw-12/sketch.js
@@ -1,13 +1,22 @@
 
+let smiles = [];
+let max = 10;
+
 function setup(){
 
   createCanvas(windowWidth, windowHeight);
     background('black');
+    for(let i = 0; i < max; i++){
+      smiles.push(new smile(random(150, width - 150), random(150, height - 150), random(25, 150), random(-5,5), random(-5,5)));
+    }
 }
 
 function draw(){
 
+  background('black');
+  for(let i = 0; i < max; i++){
+    smiles[i].frame();
+    smiles[i].collision(smiles, i);
 
-  smile = new smile(width/2,height/2,150,0,0);
-  smile.display();
+  }
 }
--- hw-12/smile.js
+++ hw-12/smile.js
@@ -5,15 +5,57 @@ class smile{
     this.width = width;
     this.deltaX = deltaX;
     this.deltaY = deltaY;
+    this.rad = this.width/2;
   }
   display(){
     push();
     noFill();
     stroke('white');
+    // translate(this.posX, this.posY);
     ellipse(this.posX,this.posY,this.width);
     arc(this.posX,this.posY,this.width/1.5,this.width/1.5, PI/8 , 7*PI/8);
-    ellipse(this.posX - this.width/2, this.posY - this.height/2, 20);
-    ellipse(this.posX + this.width/2, this.posY + this.height/2, 20);
+
+    strokeWeight(3);
+    point(this.posX - this.width/7, this.posY - this.width/7);
+    point(this.posX + this.width/7, this.posY - this.width/7);
+
     pop();
   }
+  move(){
+    this.posX += this.deltaX;
+    this.posY += this.deltaY;
+  }
+  edgeCheck() {
+       // check if the ball has hit a vertical wall (left or right walls)
+       if (this.posX + this.rad >= width || this.posX - this.rad <= 0) {
+           this.deltaX *= -1;
+
+       }
+       // check if the ball has hit a horizontal wall (top or bottom walls)
+       if (this.posY + this.rad >= height || this.posY - this.rad <= 0) {
+           this.deltaY *= -1;
+
+       }
+     }
+    collision(otherSmiles, myId){
+      // for loop touches each of the balls in the array
+        for (let n = 0; n < otherSmiles.length; n++) {
+            // if n != myId, then check for touching
+            // otherwise, its ME and we need to skip
+            if (n != myId) {
+                let d = dist(this.posX, this.posY, otherSmiles[n].posX, otherSmiles[n].posY);
+                let combinedR = this.rad + otherSmiles[n].rad;
+
+                if (d <= combinedR) {
+                    this.deltaX *= -1;
+                    this.deltaY *= -1;
+                }
+            }
+    }
+  }
+    frame(){
+      this.display();
+      this.move();
+      this.edgeCheck();
+    }
 }
