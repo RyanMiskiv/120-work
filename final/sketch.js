2ddb27e3e1e73d8256dbe39f58442bff2c15688e
RyanMiskiv <ryan.miskiv@gmail.com>
2018-12-04 20:58:26 -0700

    Most of the visual elements in place

--- final/sketch.js
+++ final/sketch.js
@@ -1,17 +1,18 @@
-let MainTV;
+let mainTV;
+let mainDial;
 
 function setup(){
 
   createCanvas(windowWidth, windowHeight);
   background('white');
-  MainTV = new tv(windowWidth/2, windowHeight/2, 800, 570);
+  mainTV = new tv(windowWidth/2, windowHeight/2, 800, 570);
 
 }
 
 function draw(){
 
   background('white');
-  MainTV.display();
+  mainTV.display();
 }
 
 class tv{
@@ -22,10 +23,10 @@ class tv{
     this.width = width;
     this.height = height;
     this.color = color(117, 47, 21);
-
+    this.dial = new dial(this.centerX + 260, this.centerY - 150, 130);
   }
   display(){
-
+push();
     noStroke();
     rectMode(CENTER);
 //main body
@@ -41,17 +42,40 @@ class tv{
 //side panel
   push();
     fill('black');
-    rect(this.centerX + 260, this.centerY, 200, 520);
+    rect(this.centerX + 260, this.centerY, 200, 500);
   pop();
 //screen
   push();
     fill('rgb(198, 195, 192)');
     rect(this.centerX - 110, this.centerY, 500, 520, 75);
   pop();
+pop();
+this.dial.displayDial();
 
   }
-  dial(){
+
+
+}
+
+class dial{
+  constructor(x,y,width){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.rotation = 0;
+  }
+  displayDial(){
+    push();
+      fill('rgb(185, 185, 185)');
+      ellipse(this.x,this.y,this.width);
+    pop();
+
+    push();
+      fill('black');
+      strokeWeight(5);
+      line(this.x, this.y, this.x, this.y - this.width/2);
+    pop();
+
 
   }
-
 }
